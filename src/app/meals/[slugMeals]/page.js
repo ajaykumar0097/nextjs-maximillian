f599a17cabe0c0b78e6bd4e6e8bc1ececb56b335
Ajay Kumar <omkashyap9536@gmail.com>
2025-08-24 12:20:13 +0530

    final chapter of over application
    Signed-off-by: Ajay Kumar <omkashyap9536@gmail.com>

--- src/app/meals/[slugMeals]/page.js
+++ src/app/meals/[slugMeals]/page.js
@@ -4,6 +4,18 @@ import Image from "next/image";
 import { getMeal } from "../../../../lib/meals";
 import { notFound } from "next/navigation";
 
+
+export async function generateMetadata({params}){
+const meal=getMeal(params.slugMeals)
+if(!meal){
+  notFound()
+}
+return {
+  title:meal.title,
+  description:meal.summary
+}
+}
+
 const MealDetails = ({params}) => {
 
   
